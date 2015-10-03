de38ab097140778ef54a8f549aac34ff055fce74
Zoffix Znet <cpan@zoffix.com>
2015-10-03 15:38:48 -0400

    First full styling

--- source/js.js
+++ source/js.js
@@ -12,4 +12,20 @@ jQuery(function ($) {
     $('.home section:eq(0) .panel-body, .home section:eq(1) .panel-body').sameHeight();
     $('footer ul').sameHeight();
     $('.equal-panels .panel-body').sameHeight();
+
+    setup_chat_button();
+    $(window).resize(setup_chat_button);
 });
+
+function setup_chat_button() {
+    var el = $('#chat');
+    if ( ! el.length ) { return; }
+
+    var cont = $('.bg > .container');
+    el.css({
+        left: cont.offset().left + cont.innerWidth() - el.outerWidth() - 15
+    });
+
+    // el.css({opacity: 1});
+    setTimeout(function(){el.animate({bottom: '-10px'});}, 1000);
+}
