77bc105ec4f8462be0da786251dbc358fc4f7e6d
Zoffix Znet <cpan@zoffix.com>
2018-03-27 00:19:30 -0400

    Start sketching home page carousel

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,14 @@
 $(function(){
+    setup_carousels();
 });
+
+function setup_carousels() {
+    $('.carousel').carousel();
+
+    var max_height = 0;
+    $('.carousel .carousel-item').each(function(){
+        var h = $(this).height();
+        if (h > max_height) max_height = h;
+    });
+    $('.carousel .carousel-item').css({height: max_height + 'px'});
+}
