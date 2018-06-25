4c84c5ea2ba6602280ee8fb5f234b9ac3728e749
Zoffix Znet <cpan@zoffix.com>
2018-06-25 07:10:15 -0400

    Get rid of comments on medium.com showing up as new blogs

--- fetch-recent-blog-posts.pl
+++ fetch-recent-blog-posts.pl
@@ -14,8 +14,12 @@ unless ( $tx->success ) {
     exit;
 }
 
-my $j = $tx->res->dom->find("entry")->slice(0..6)
-->map(sub{
+my $j = $tx->res->dom->find("entry")->grep(sub {
+    # hackish fix to get rid of comment entries from medium.com
+    $_->at('link')->{href} =~ m{^https://medium\.com}
+        ? $_->find('category')->each
+        : 1
+    })->slice(0..6)->map(sub{
         # Look for text/html links and use the first without it if we don't find any
         my $link = $_->at('link[type="text/html"]:not([rel="replies"])')
             // $_->at('link:not([rel="replies"])');
