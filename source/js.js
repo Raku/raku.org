0654e8cb38a34002599f8ca23a630b0b491dbff4
Will Coleda <will@coleda.com>
2023-11-05 00:15:10 -0400

    Remove some (boilerplate?) JS?
    Part of #204

--- source/js.js
+++ source/js.js
@@ -1,27 +1,6 @@
 jQuery(function ($) {
-    $.fn.sameHeight = function() {
-        var max = 0;
-        $(this).each(function(){
-            var h = $(this).outerHeight();
-            if ( h > max ) { max = h }
-        });
-
-        $(this).css('min-height', max + 'px');
-    }
-
-    $('.home section:eq(0) .panel-body, .home section:eq(1) .panel-body').sameHeight();
-    $('footer ul').sameHeight();
-    $('.equal-panels .panel-body').sameHeight();
-
-    if ( ! $('.bg').length ) { $('body').css({background: '#fff'}) }
-
-    setup_recent_blog_posts();
-});
-
-function setup_recent_blog_posts () {
     var el = $('#recent_blog_posts');
     if ( ! el.length ) { return; }
-
 
     $.ajax('recent-blog-posts.json')
     .done(function(feed) {
@@ -35,4 +14,4 @@ function setup_recent_blog_posts () {
     .fail(function() {
         el.append('<li><i>Failed to fetch recent blogs</i></li>');
     });
-}
+});
