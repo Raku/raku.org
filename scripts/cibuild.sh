628659896bfbd99a2760b3fba623884d663d57e9
Coleman McFarland <43583445+dontlaugh@users.noreply.github.com>
2026-02-14 16:38:43 -0500

    use new container path (#294)
    Refactor cibuild.sh script
    
    Remove reference to proto-25 container repository. This will let
    us remove that repository completely and move to a more unambiguous
    name.

--- scripts/cibuild.sh
+++ scripts/cibuild.sh
@@ -3,36 +3,31 @@
 set -e
 
 cmd_container () {
-  # https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/store-information-in-variables#default-environment-variables
+  # Create a calendar-sortable string as a container version number
   tag_version="v1-$(date +%Y%m%d)-${GITHUB_RUN_NUMBER}"
 
   # Only login to quay.io if we're going to push (on main or dev branch)
   if [ "$GITHUB_REF_NAME" = "main" ] || [ "$GITHUB_REF_NAME" = "dev" ]; then
     echo $QUAY_PASSWORD | docker login quay.io -u $QUAY_USERNAME --password-stdin
   fi
-  
-  full_tag="quay.io/chroot.club/proto-25:${tag_version}"
-  docker build --build-arg quay_expiration="8w" -t $full_tag .
 
-  # Also tag as "latest"
-  latest_tag="quay.io/chroot.club/proto-25:latest"
+  # The "full tag" is a calendar-sortable version
+  full_tag="quay.io/chroot.club/raku-org-website:${tag_version}"
+  docker build --build-arg quay_expiration="24w" -t $full_tag .
+
+  # Also tag this build as "latest"
+  latest_tag="quay.io/chroot.club/raku-org-website:latest"
   docker tag $full_tag $latest_tag
-
-  # Begin migration to non-prototype tag
-  raku_org_full_tag="quay.io/chroot.club/raku-org-website:${tag_version}"
-  raku_org_latest_tag="quay.io/chroot.club/raku-org-website:latest"
-  docker tag $full_tag $raku_org_full_tag
-  docker tag $full_tag $raku_org_latest_tag
 
   # Only push if on main or dev branch
   if [ "$GITHUB_REF_NAME" = "main" ]; then
     echo "Pushing container images (branch: $GITHUB_REF_NAME)"
+    # Push both full tag and latest
     docker push $full_tag
     docker push $latest_tag
-    docker push $raku_org_full_tag
-    docker push $raku_org_latest_tag
   elif [ "$GITHUB_REF_NAME" = "dev" ]; then
     echo "Pushing dev container images (branch: $GITHUB_REF_NAME)"
+    # Only push and overwrite the "dev" tag in this case
     dev_tag="quay.io/chroot.club/raku-org-website:dev"
     docker tag $full_tag $dev_tag
     docker push $dev_tag
@@ -56,3 +51,4 @@ case $CMD in
     exit 1
     ;;
 esac
+
